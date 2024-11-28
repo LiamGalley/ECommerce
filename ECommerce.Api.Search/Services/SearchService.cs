f6af34c95117ab6229ffb02bcb8c37eb4bc40e4a
Liam Galley <119807591+LiamGalley@users.noreply.github.com>
2024-11-28 13:58:11 -0500

    Completed customer invokation in search

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -8,26 +8,36 @@ namespace ECommerce.Api.Search.Services
     {
         private readonly IOrdersService ordersService;
         private readonly IProductsService productsService;
+        private readonly ICustomerService customerService;
 
-        public SearchService(IOrdersService ordersService, IProductsService productsService)
+        public SearchService(IOrdersService ordersService, IProductsService productsService, ICustomerService customerService)
         {
             this.ordersService = ordersService;
             this.productsService = productsService;
+            this.customerService = customerService;
         }
 
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
             var ordersResult = await ordersService.GetOrdersAsync(customerId);
             var productsResult = await productsService.GetProductsAsync();
+            var customerResult = await customerService.GetCustomerAsync(customerId);
 
             if (ordersResult.IsSuccess)
             {
                 foreach (var order in ordersResult.Orders)
                     foreach (var item in order.Items)
-                        item.ProductName = productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name;
+                    {
+                        item.ProductName = productsResult.IsSuccess ?
+                            productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name :
+                            "Product Information Is Not Available";
+                    }
 
                 var result = new
                 {
+                    Customer = customerResult.IsSuccess ?
+                        customerResult.Customer :
+                        new { Name = "Customer Information Is Not Available" },
                     Orders = ordersResult.Orders,
                 };
                 return (true, result);
