96a1f8e45db1a7943393fee40e7a1d1bbafbec59
Liam Galley <119807591+LiamGalley@users.noreply.github.com>
2024-11-27 21:24:34 -0500

    Added customer microservice

---
+++ ECommerce.Api.Customers/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Api.Customers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomersProvider _customersProvider;
+
+        public CustomerController(ICustomersProvider customersProvider)
+        {
+            this._customersProvider = customersProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomersAsync()
+        {
+            var result = await _customersProvider.GetCustomersAsync();
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Customers);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerAsync(int id)
+        {
+            var result = await _customersProvider.GetCustomerAsync(id);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Customer);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ECommerce.Api.Customers/Providers/CustomerProvider.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Providers
+{
+    public class CustomerProvider : ICustomersProvider
+    {
+        private readonly CustomersDbContext dbContext;
+        private readonly ILogger<CustomerProvider> logger;
+        private readonly IMapper mapper;
+
+        public CustomerProvider(CustomersDbContext dbContext, ILogger<CustomerProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!this.dbContext.Customers.Any())
+            {
+                dbContext.Customers.Add(new Db.Customer() { Id = 1, Name = "Claudiu Sensei", Address = "1234 Maple" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 2, Name = "Aref Sensei", Address = "234 Apple" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 3, Name = "Vik Sensei", Address = "432 Cedar" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 4, Name = "Zahra Sensei", Address = "876 Ebony" });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, Models.Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                var customer = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+
+                if (customer != null)
+                {
+                    var result = mapper.Map<Db.Customer, Models.Customer>(customer);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            } catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Customer> Customers, string ErrorMessage)> GetCustomersAsync()
+        {
+            try
+            {
+                var customers = await dbContext.Customers.ToListAsync();
+
+                if (customers != null && customers.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Customer>, IEnumerable<Models.Customer>>(customers);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
